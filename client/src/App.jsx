0505bcd01b50424b9e595c40a80de9d29509552b
Yiannis Kalogiannis <kalojohn22@gmail.com>
2025-01-16 20:57:21 +0200

    install tailwind and daisyUI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
+import Navbar from "./components/Navbar";
+
+
 function App() {
   return ( 
-    <p>test</p>
+    <>
+    <Navbar />
+    <button className="btn btn-outline">Default</button>
+    <button className="btn btn-outline btn-primary">Primary</button>
+    <button className="btn btn-outline btn-secondary">Secondary</button>
+    <button className="btn btn-outline btn-accent">Accent</button>
+    </>
    );
 }
 
